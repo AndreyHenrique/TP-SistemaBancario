2d0d5708e1ca020e41cef956e646410dff0735b6
Andrey Henrique <127218100+AndreyHenrique@users.noreply.github.com>
2026-04-16 08:54:43 -0400

    Refactor Conta class and add Banco class

--- src/banksys/br/ufc/dc/tp1/conta/Conta.java
+++ src/banksys/br/ufc/dc/tp1/conta/Conta.java
@@ -1,31 +1,89 @@
-package banksys.br.ufc.dc.tp1.conta;
+package banksys.br.ufc.dc.tp1.banco;
 
-public class Conta {
-    private String numero;
-    private double saldo;
+import banksys.br.ufc.dc.tp1.conta.Conta;
+import banksys.br.ufc.dc.tp1.conta.ContaEspecial;
+import banksys.br.ufc.dc.tp1.conta.ContaPoupanca;
 
-    public Conta (String numero) {
-        this.numero = numero;
-        saldo = 0;
+public class Banco {
+    private Conta[] contas;
+    private int indice = 0;
+
+    public Banco() {
+        contas = new Conta[100];
     }
 
-    public void creditar (double valor) {
-        this.saldo += valor;
+    public void cadastrar(Conta conta) {
+        contas[indice] = conta;
+        indice ++;
     }
 
-    public void debitar (double valor) {
-        this.saldo -= valor;
+    private Conta procurar(String numeroConta) {
+        int i = 0;
+        boolean achou = false;
+
+        while ((!achou) && (i < indice)) {
+            String numeroContaAchada = contas[i].getNumero();
+            if (numeroContaAchada.equals(numeroConta)) {
+                achou = true;
+            }
+            else {
+                i ++;
+            }
+        }
+
+        if (achou){
+            return contas[i];
+        }
+        else { return null; }
     }
 
-    public void mostrarConta() {
-        System.out.println("O saldo da conta " + getNumero() + " é de: R$" + getSaldo());
+    public void debitar(String numero, double valor) {
+        Conta conta = procurar(numero);
+        if (conta != null) {
+            conta.debitar(valor);
+        }
+        else System.out.println("A conta não existe.");
     }
 
-    public String getNumero() {
-        return this.numero;
+    public void creditar(String numero, double valor) {
+        Conta conta = procurar(numero);
+        if (conta != null) {
+            conta.creditar(valor);
+        }
+        else System.out.println("A conta não existe.");
     }
 
-    public double getSaldo() {
-        return this.saldo;
+    public void transferir(String numeroA, String numeroB, double valor) {
+        Conta contaA = procurar(numeroA);
+        Conta contaB = procurar(numeroB);
+
+        if (contaA == null || contaB == null) {
+            System.out.println("A conta não existe");
+        }
+        debitar(numeroA, valor);
+        creditar(numeroB, valor);
+    }
+    
+    public void renderJuros(String numero) {
+    	Conta contaAchada = this.procurar(numero);
+    	if (contaAchada instanceof ContaPoupanca)
+    	{
+    		((ContaPoupanca) contaAchada).renderJuros(0.01);
+    	}
+    	else {
+    		System.out.println("Conta não é conta de poupança.");
+    	}
+    	
+    }
+    
+    public void renderBonus(String numero) {
+    	Conta contaAchada = this.procurar(numero);
+    	if (contaAchada instanceof ContaEspecial)
+    	{
+    		((ContaEspecial) contaAchada).renderBonus();
+    	}
+    	else {
+    		System.out.println("Conta não é conta especial.");
+    	}
     }
 }
