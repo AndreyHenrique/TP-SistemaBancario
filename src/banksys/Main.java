224268754a12ac6804b3553dd0be4bfe74d2cc2d
Andrey Henrique <127218100+AndreyHenrique@users.noreply.github.com>
2026-04-16 08:54:19 -0400

    Refactor Main class to test

--- src/banksys/Main.java
+++ src/banksys/Main.java
@@ -1,29 +1,50 @@
 package banksys;
 
+import banksys.br.ufc.dc.tp1.banco.Banco;
 import banksys.br.ufc.dc.tp1.banco.BancoVector;
+
 import banksys.br.ufc.dc.tp1.conta.Conta;
+import banksys.br.ufc.dc.tp1.conta.ContaEspecial;
 import banksys.br.ufc.dc.tp1.conta.ContaPoupanca;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
         ContaPoupanca c = new ContaPoupanca("12345");
         Conta d = new Conta("11111");
 
-        BancoVector banco_a = new BancoVector();
+        Banco banco_a = new Banco(); 
+        
         banco_a.cadastrar(c);
         banco_a.cadastrar(d);
-
         banco_a.creditar("12345", 1000);
         banco_a.creditar("11111", 500);
         banco_a.debitar("11111", 100);
         banco_a.transferir("11111", "12345", 400);
+        
+        
 
         c.renderJuros(0.01);
 
         c.mostrarConta();
         d.mostrarConta();
+        
+        
+        // teste de juros e bonus
+        BancoVector bancoB = new BancoVector();
+        ContaPoupanca contaPoupanca = new ContaPoupanca("12");
+        ContaEspecial contaEspecial = new ContaEspecial("21");
+        
+        bancoB.cadastrar(contaPoupanca);
+        bancoB.cadastrar(contaEspecial);
+        
+        bancoB.creditar("12", 10000);
+        bancoB.creditar("21", 10000);
+        
+        bancoB.renderJuros("12");
+        bancoB.renderBonus("21");
+        
+        contaPoupanca.mostrarConta();
+        contaEspecial.mostrarConta();
     }
 }
