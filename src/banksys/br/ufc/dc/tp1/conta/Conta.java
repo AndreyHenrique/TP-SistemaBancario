c354f77ebaa0bdda9dd8e9d5af0fdc806d3e3ebd
Andrey Henrique <127218100+AndreyHenrique@users.noreply.github.com>
2026-04-23 08:38:43 -0400

    Refactor Conta class to extend ContaAbstrata
    Refactored Conta class to extend ContaAbstrata and updated constructor. Removed unused methods and variables.

--- src/banksys/br/ufc/dc/tp1/conta/Conta.java
+++ src/banksys/br/ufc/dc/tp1/conta/Conta.java
@@ -1,31 +1,12 @@
 package banksys.br.ufc.dc.tp1.conta;
 
-public class Conta {
-    private String numero;
-    private double saldo;
+public class Conta extends ContaAbstrata {
 
     public Conta (String numero) {
-        this.numero = numero;
-        saldo = 0;
+        super(numero);
     }
-
-    public void creditar (double valor) {
-        this.saldo += valor;
-    }
-
+    
     public void debitar (double valor) {
-        this.saldo -= valor;
-    }
-
-    public void mostrarConta() {
-        System.out.println("O saldo da conta " + getNumero() + " é de: R$" + getSaldo());
-    }
-
-    public String getNumero() {
-        return this.numero;
-    }
-
-    public double getSaldo() {
-        return this.saldo;
+        saldo -= valor;
     }
 }
